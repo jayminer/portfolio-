a0471a37930fc275682a3ce7512582b1a2292b2e
max kanter <max@maxkanter.(none)>
2011-08-04 00:16:21 -0700

    basic structure done

---
+++ js/script.js
@@ -0,0 +1,113 @@
+var app = $.sammy('#main', function() {
+	this.swap = function(content) {
+		this.$element().hide(0).html(content).fadeIn(300);
+	}
+
+	this.get('#/me', function(context) {
+		$('#project-list').hide('slow');
+		var html = $('#me').html();
+		this.swap(html);
+	});
+	
+	this.get('#/contact', function(context) {
+		$('#project-list').hide('slow');
+		var html = $('#contact').html();
+		this.swap(html);
+	});
+	
+	this.get('#/resume', function(context) {
+		$('#project-list').hide('slow');
+		var html = $('#resume').html();
+		this.swap(html);
+	});
+	
+	this.get('#/projects', function(context) {
+		var html = $('#projects').html(), content, categories = [], newCategories, $checkboxContainer;
+		$('#project-list').show('slow');
+		this.swap(html);
+		/*
+		$checkboxContainer = $('.checkbox-container').html('');;*/
+		content = this.$element().find('.content');
+		var count=0
+		$('#data-hidden > .project').each(function(){
+			
+			var $this = $(this),
+			title = $this.attr('title');/*,
+			newCategories = $this.attr('category').split(' '),
+			$checkbox;
+			
+			
+			var i = newCategories.length;
+			while (i>0) {
+				tempCat = newCategories[i-1];
+				if (categories.indexOf(tempCat)==-1){
+					categories.push(tempCat);					
+					$checkboxContainer.append('<div id="'+tempCat+'-checkbox" class="checkbox checkbox-enabled">'+tempCat+'</div>');
+					console.log();
+					var checkBox = new Checkbox(tempCat+'-checkbox');
+				}
+				i--
+			}	*/
+			
+			image = $this.children('.first-image').eq(0).attr('src');
+				content.append('<a href="#/projects/' + escape(title) + '"><div class="feature-container '+$this.attr('category')+'" style="opacity:0; background:#'+Math.floor(Math.random()*16777215).toString(16)+' no-repeat;">' +
+				'<div class="feature-title">'+title+'</div>' + 
+				'<div class="feature-image" style="background:url(\''+image+'\') no-repeat;"></div>' +
+				'</div></a>')
+		})
+		
+		$('.feature-container').each(function(){
+			var $this = $(this);
+			setTimeout(function(){
+					$this.css('opacity','1');
+			},Math.random()*1000)
+		});
+		
+		
+		
+		
+
+		
+	});
+	
+	this.get('#/projects/:title', function(context) {
+		var title = unescape(this.params['title']).replace(' ',"");;
+		$('#project-list').show('slow');
+		html = '<div class="title">' + this.params['title'] + '</div>' + $('#' + title).html();
+		console.log(html);
+		this.swap(html);
+	});
+	
+	this.notFound('get','#/me');
+});
+
+$(document).ready(function() {
+	$('#data-hidden').load('templates/templates.html', function () {
+		$('#project-list').html('');
+		$('#data-hidden > .project').each(function(){
+			var title = $(this).attr('title');
+			$('#project-list').append('<li><a href="#/projects/' + escape(title) + '">' + title +'</a></li>');
+		})
+		app.run('#/me');
+	});
+	
+});
+
+function Checkbox (id) {
+	self = this;
+	self.$div = $('#'+id);;
+	self.enabled = true;
+	self.toggle = function ($div) {
+			console.log($div.parent().parent().children('.content').find('a>div'));
+			$div.parent().parent().children('.content').find('a>div').filter('.'+$div.text()).toggle();
+			self.enabled
+			self.enabled = (self.enabled) ? false:true;
+			$div.toggleClass('checkbox-enabled')
+	}
+	self.$div.click(function() {
+		self.toggle($(this));
+	});
+	
+	return self;
+}
+
